912a4701afb3585df040b1c9ae6c028e383edeb8
Matteo <matteo.bertola77@gmail.com>
2019-12-29 18:24:51 +0100

    add code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SortDutchProblem
-Dutch national flag problem
+Implementation of the Dutch national flag problem.
+
+**Language: C#**
+
+**Start: 2015**
+
+## Why
+I stumbled into the problem of the [Dutch national flag problem](https://en.wikipedia.org/wiki/Dutch_national_flag_problem) and I wanted to find my own solution.
---
+++ SortDutchProblem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortDutchProblem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] xs = new int[] {2, 0, 1, 2};
+      SortDutchProblem(xs);
+      xs = new int[] { 0, 1, 2, 0, 2, 2, 1, 0, 0, 2, 0, 0, 2 };
+      SortDutchProblem(xs);
+    }
+
+    private static void SortDutchProblem(int[] xs)
+    {
+      int count = xs.Length;
+      int i = 0;
+      int last1 = -1;
+      while (i < count)
+      {
+        int x = xs[i];
+        if (x == 0)
+        {
+          i++;
+          continue;
+        }
+
+        //x == 1 || 2
+        count--;
+        swap(xs, i, count);
+
+        if (x == 1)
+        {
+          if (last1 == -1) last1 = count;
+          continue;
+        }
+
+        //x == 2
+        if (last1 > -1)
+        {
+          swap(xs, count, last1);
+          last1 = count;
+        }
+      }
+    }
+
+    private static void swap(int[] xs, int i, int j)
+    {
+      int tmp = xs[i];
+      xs[i] = xs[j];
+      xs[j] = tmp;
+    }
+  }
+}
